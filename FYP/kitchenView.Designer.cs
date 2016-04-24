574aad153cee2a346a64cc21692342945fe0cd87
wongpuiho0929 <wongpuiho0929@gmail.com>
2016-04-25 03:01:42 +0800

    panel -- KV

--- FYP/kitchenView.Designer.cs
+++ FYP/kitchenView.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.gb1 = new System.Windows.Forms.GroupBox();
             this.gb_OrderStatus = new System.Windows.Forms.GroupBox();
             this.btn_print = new System.Windows.Forms.Button();
             this.gbFunction = new System.Windows.Forms.GroupBox();
@@ -39,32 +38,22 @@
             this.lbl_tov = new System.Windows.Forms.Label();
             this.FLP1 = new System.Windows.Forms.FlowLayoutPanel();
             this.FLP2 = new System.Windows.Forms.FlowLayoutPanel();
-            this.gb1.SuspendLayout();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.gb_OrderStatus.SuspendLayout();
             this.gbFunction.SuspendLayout();
             this.gb_Information.SuspendLayout();
             this.FLP1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // gb1
-            // 
-            this.gb1.Controls.Add(this.gb_OrderStatus);
-            this.gb1.Controls.Add(this.gbFunction);
-            this.gb1.Controls.Add(this.gb_foobType);
-            this.gb1.Controls.Add(this.gb_Information);
-            this.gb1.Location = new System.Drawing.Point(1206, 13);
-            this.gb1.Name = "gb1";
-            this.gb1.Size = new System.Drawing.Size(170, 660);
-            this.gb1.TabIndex = 1;
-            this.gb1.TabStop = false;
-            this.gb1.Text = "filter";
             // 
             // gb_OrderStatus
             // 
             this.gb_OrderStatus.Controls.Add(this.btn_print);
-            this.gb_OrderStatus.Location = new System.Drawing.Point(7, 528);
+            this.gb_OrderStatus.Location = new System.Drawing.Point(4, 605);
+            this.gb_OrderStatus.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.gb_OrderStatus.Name = "gb_OrderStatus";
-            this.gb_OrderStatus.Size = new System.Drawing.Size(200, 100);
+            this.gb_OrderStatus.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.gb_OrderStatus.Size = new System.Drawing.Size(267, 115);
             this.gb_OrderStatus.TabIndex = 5;
             this.gb_OrderStatus.TabStop = false;
             this.gb_OrderStatus.Text = "Print";
@@ -72,9 +61,10 @@
             // btn_print
             // 
             this.btn_print.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_print.Location = new System.Drawing.Point(4, 13);
+            this.btn_print.Location = new System.Drawing.Point(5, 15);
+            this.btn_print.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btn_print.Name = "btn_print";
-            this.btn_print.Size = new System.Drawing.Size(143, 81);
+            this.btn_print.Size = new System.Drawing.Size(191, 93);
             this.btn_print.TabIndex = 0;
             this.btn_print.Text = "Print";
             this.btn_print.UseVisualStyleBackColor = true;
@@ -84,9 +74,11 @@
             // 
             this.gbFunction.Controls.Add(this.btn_quickPrint);
             this.gbFunction.Controls.Add(this.btn_TimeClear);
-            this.gbFunction.Location = new System.Drawing.Point(7, 140);
+            this.gbFunction.Location = new System.Drawing.Point(4, 158);
+            this.gbFunction.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.gbFunction.Name = "gbFunction";
-            this.gbFunction.Size = new System.Drawing.Size(138, 242);
+            this.gbFunction.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.gbFunction.Size = new System.Drawing.Size(184, 279);
             this.gbFunction.TabIndex = 1;
             this.gbFunction.TabStop = false;
             this.gbFunction.Text = "Take Time";
@@ -94,9 +86,10 @@
             // btn_quickPrint
             // 
             this.btn_quickPrint.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_quickPrint.Location = new System.Drawing.Point(71, 160);
+            this.btn_quickPrint.Location = new System.Drawing.Point(95, 185);
+            this.btn_quickPrint.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btn_quickPrint.Name = "btn_quickPrint";
-            this.btn_quickPrint.Size = new System.Drawing.Size(75, 67);
+            this.btn_quickPrint.Size = new System.Drawing.Size(100, 77);
             this.btn_quickPrint.TabIndex = 1;
             this.btn_quickPrint.Text = "Quick Group by time Print";
             this.btn_quickPrint.UseVisualStyleBackColor = true;
@@ -105,9 +98,10 @@
             // btn_TimeClear
             // 
             this.btn_TimeClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_TimeClear.Location = new System.Drawing.Point(71, 19);
+            this.btn_TimeClear.Location = new System.Drawing.Point(95, 22);
+            this.btn_TimeClear.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btn_TimeClear.Name = "btn_TimeClear";
-            this.btn_TimeClear.Size = new System.Drawing.Size(59, 86);
+            this.btn_TimeClear.Size = new System.Drawing.Size(79, 99);
             this.btn_TimeClear.TabIndex = 0;
             this.btn_TimeClear.Text = "Clear time";
             this.btn_TimeClear.UseVisualStyleBackColor = true;
@@ -115,9 +109,11 @@
             // 
             // gb_foobType
             // 
-            this.gb_foobType.Location = new System.Drawing.Point(17, 388);
+            this.gb_foobType.Location = new System.Drawing.Point(18, 444);
+            this.gb_foobType.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.gb_foobType.Name = "gb_foobType";
-            this.gb_foobType.Size = new System.Drawing.Size(137, 145);
+            this.gb_foobType.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.gb_foobType.Size = new System.Drawing.Size(183, 167);
             this.gb_foobType.TabIndex = 4;
             this.gb_foobType.TabStop = false;
             this.gb_foobType.Text = "Food Type";
@@ -125,9 +121,11 @@
             // gb_Information
             // 
             this.gb_Information.Controls.Add(this.lbl_tov);
-            this.gb_Information.Location = new System.Drawing.Point(7, 19);
+            this.gb_Information.Location = new System.Drawing.Point(4, 18);
+            this.gb_Information.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.gb_Information.Name = "gb_Information";
-            this.gb_Information.Size = new System.Drawing.Size(137, 104);
+            this.gb_Information.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.gb_Information.Size = new System.Drawing.Size(183, 120);
             this.gb_Information.TabIndex = 3;
             this.gb_Information.TabStop = false;
             this.gb_Information.Text = "Information";
@@ -135,9 +133,10 @@
             // lbl_tov
             // 
             this.lbl_tov.AutoSize = true;
-            this.lbl_tov.Location = new System.Drawing.Point(7, 20);
+            this.lbl_tov.Location = new System.Drawing.Point(9, 23);
+            this.lbl_tov.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_tov.Name = "lbl_tov";
-            this.lbl_tov.Size = new System.Drawing.Size(93, 13);
+            this.lbl_tov.Size = new System.Drawing.Size(115, 15);
             this.lbl_tov.TabIndex = 0;
             this.lbl_tov.Text = "Total Order Value:";
             // 
@@ -146,46 +145,60 @@
             this.FLP1.AutoScroll = true;
             this.FLP1.Controls.Add(this.FLP2);
             this.FLP1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.FLP1.Location = new System.Drawing.Point(12, 13);
+            this.FLP1.Location = new System.Drawing.Point(16, 15);
+            this.FLP1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.FLP1.Name = "FLP1";
-            this.FLP1.Size = new System.Drawing.Size(735, 489);
+            this.FLP1.Size = new System.Drawing.Size(980, 564);
             this.FLP1.TabIndex = 2;
             // 
             // FLP2
             // 
             this.FLP2.AutoScroll = true;
             this.FLP2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.FLP2.Location = new System.Drawing.Point(3, 3);
+            this.FLP2.Location = new System.Drawing.Point(4, 3);
+            this.FLP2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.FLP2.Name = "FLP2";
-            this.FLP2.Size = new System.Drawing.Size(735, 489);
+            this.FLP2.Size = new System.Drawing.Size(980, 564);
             this.FLP2.TabIndex = 3;
             this.FLP2.Visible = false;
             // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.gb_OrderStatus);
+            this.panel1.Controls.Add(this.gb_Information);
+            this.panel1.Controls.Add(this.gbFunction);
+            this.panel1.Controls.Add(this.gb_foobType);
+            this.panel1.Location = new System.Drawing.Point(1707, 18);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 759);
+            this.panel1.TabIndex = 3;
+            // 
             // kitchenView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1517, 796);
-            this.Controls.Add(this.gb1);
+            this.ClientSize = new System.Drawing.Size(1932, 918);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.FLP1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "kitchenView";
             this.Text = "Form1";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.kitchenView_Load);
-            this.gb1.ResumeLayout(false);
             this.gb_OrderStatus.ResumeLayout(false);
             this.gbFunction.ResumeLayout(false);
             this.gb_Information.ResumeLayout(false);
             this.gb_Information.PerformLayout();
             this.FLP1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox gb1;
         private System.Windows.Forms.FlowLayoutPanel FLP1;
         private System.Windows.Forms.GroupBox gb_Information;
         private System.Windows.Forms.Label lbl_tov;
@@ -196,5 +209,6 @@
         private System.Windows.Forms.Button btn_print;
         private System.Windows.Forms.Button btn_TimeClear;
         private System.Windows.Forms.Button btn_quickPrint;
+        private System.Windows.Forms.Panel panel1;
     }
 }
